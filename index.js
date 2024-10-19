94abd05a3612e441bf6f18469d19ee29d7f2be0d
Navneet Kumar Yadav <92726948+Navneet170904@users.noreply.github.com>
2024-10-19 11:50:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const fs = require('fs');
+
+
+function basedecoder(value, base) {
+    return parseInt(value, base);
+}
+
+
+function lagrangeInterpolation(numbers) {
+    let fixed = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        let [x_i, y_i] = numbers[i];
+        let phase = y_i;
+
+        for (let j = 0; j < numbers.length; j++) {
+            if (i !== j) {
+                let [x_j] = numbers[j];
+                phase *= x_j / (x_j - x_i);
+            }
+        }
+
+        fixed += phase;
+    }
+
+    return Math.round(fixed);
+}
+
+
+function solvedequation(filename) {
+    const content = fs.readFileSync(filename);
+    const input = JSON.parse(content);
+
+    const { n, k } = input.keys;
+    let numbers = [];
+
+    Object.keys(input).forEach(key => {
+        if (!isNaN(key)) {
+            const base = parseInt(input[key].base, 10);
+            const value = input[key].value;
+            const x = parseInt(key, 10);
+            const y = basedecoder(value, base);
+
+            numbers.push([x, y]);
+        }
+    });
+    const requirednumbers = numbers.slice(0, k);
+
+    const constantTerm = lagrangeInterpolation(requirednumbers);
+
+    console.log(`The secret fixed phase (c) is: ${constantTerm}`);
+}
+
+
+solvedequation('tcase1.json');
+solvedequation('tcase2.json');
